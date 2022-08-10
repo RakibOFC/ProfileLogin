4dc3f8bc95af6ea637a6c81d04ad44745a057758
RakibOFC <rakibull2000@gmail.com>
2022-08-10 23:17:08 +0600

    Update all project

--- app/src/main/java/com/rakibofc/profilelogin/ProfileActivity.java
+++ app/src/main/java/com/rakibofc/profilelogin/ProfileActivity.java
@@ -6,10 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
